02e05e965d94886e7ffb20db0fbbcc3eabbda583
devstream <devstream@merico.dev>
2023-06-17 13:30:08 +0000

    init by devstream

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.New()
+	router.GET("/", rootHandler)
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8080"
+	}
+	router.Run(fmt.Sprintf(":%s", port))
+}
+
+func rootHandler(ctx *gin.Context) {
+	output := "This is silly. Convert it into something great!"
+	ctx.String(http.StatusOK, output)
+}
